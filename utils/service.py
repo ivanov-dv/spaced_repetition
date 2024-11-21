cd71d190b30b523f24e477523d06e9a5bc806915
Denis Ivanov <dnb8866@gmail.com>
2024-11-21 19:45:01 +0700

    refactor code

--- utils/service.py
+++ utils/service.py
@@ -9,11 +9,36 @@ from utils.repositories import UserRepository, RequestRepository
 
 
 class Monitoring:
-    def __init__(self, user_repo: UserRepository, request_repo: RequestRepository, bot: Bot):
+    def __init__(
+            self,
+            user_repo: UserRepository,
+            request_repo: RequestRepository,
+            bot: Bot
+    ):
         self.user_repo = user_repo
         self.request_repo = request_repo
         self.bot = bot
         self.count = 0
+
+    async def _send_message(self, request):
+        try:
+            await self.bot.send_message(
+                request.user_id,
+                f'🔊 Повторить 📖:\n\n'
+                f'{request.text}\n\n'
+                f'Следующее напоминание: '
+                f'{request.date_notice.strftime("%d.%m.%Y")}',
+                reply_markup=KB.remove_notice())
+        except Exception as e:
+            logging.error(
+                f'Ошибка отправки уведомления для '
+                f'пользователя {request.user_id}: {str(e)}'
+            )
+    async def _check_request(self, request):
+        if request.date_notice <= datetime.utcnow().date():
+            if datetime.utcnow().hour >= 9:
+                await request.calculate_next_date_notice()
+                self._send_message(request)
 
     async def check(self):
         while True:
@@ -21,20 +46,5 @@ class Monitoring:
             print('Цикл: ', self.count)
             user_requests = await self.request_repo.get_all_requests()
             for request in user_requests:
-                if request.date_notice <= datetime.utcnow().date():
-                    if datetime.utcnow().hour >= 9:
-                        await request.calculate_next_date_notice()
-                        try:
-                            await self.bot.send_message(
-                                request.user_id,
-                                f'🔊 Повторить 📖:\n\n'
-                                f'{request.text}\n\n'
-                                f'Следующее напоминание: {request.date_notice.strftime("%d.%m.%Y")}',
-                                reply_markup=KB.remove_notice())
-                        except Exception as e:
-                            print(f'Ошибка отправки уведомления для пользователя {request.user_id}: {str(e)}')
-                            logging.error(f'Ошибка отправки уведомления для пользователя {request.user_id}: {str(e)}')
-                            continue
-                        else:
-                            await self.request_repo.update(request)
+                self._check_request(request)
             await asyncio.sleep(60*60*6)
